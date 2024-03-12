c749f0d1d44a357ba6f17e2e721bf8139febdbae
Glen Hughes <ghughes@crossriver.com>
2024-03-12 11:42:11 -0400

    Logging

--- ConsoleContainer.WorkerService/Hubs/ProcessHub.cs
+++ ConsoleContainer.WorkerService/Hubs/ProcessHub.cs
@@ -5,12 +5,32 @@ using System.Threading.Channels;
 namespace ConsoleContainer.WorkerService.Hubs
 {
     public class ProcessHub(
+        ILogger<ProcessHub> logger,
         IOutputDataChannelReader outputDataChannelReader
     ) : Hub<IProcessHubSubscription>
     {
         public ChannelReader<ProcessOutputDataDto> ProcessOutputDataStream()
         {
             return outputDataChannelReader.GetChannelReader();
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            logger.LogInformation("Connection initiated");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            if (exception is null)
+            {
+                logger.LogInformation("Connection disconnected");
+            }
+            else
+            {
+                logger.LogError(exception, "Connection disconnected");
+            }
+            return base.OnDisconnectedAsync(exception);
         }
     }
 }
